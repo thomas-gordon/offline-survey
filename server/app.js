6242c96f6adfce2fcbe3d9f97ec30d098d2bb457
Tom Gordon <tom.gordon@squareweave.com.au>
2016-02-21 15:21:21 +1100

    Attempt to launch correctly

--- server/app.js
+++ server/app.js
@@ -42,6 +42,7 @@ app.post('/save', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port,function () {
+    console.log('Example app is a runnin');
 });
